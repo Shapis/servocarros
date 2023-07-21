474c1a538329d0f0d4629f0934dddbbe0e44603b
Henrique da Silva <hpsilva@pm.me>
2023-07-21 00:02:39 -0300

    Fix exports

--- Scripts/carro.cs
+++ Scripts/carro.cs
@@ -3,28 +3,32 @@ using System;
 
 public partial class carro : Node2D
 {
+    #region Inputs
     private void OnImpulso(int left, int right)
     {
         // GD.Print("Impulso: ", left, " ", right);
     }
+    #endregion
 
+    #region Exports
     [Export]
     private float _diameter = 1;
 
     [Export]
-    private float _distanciaEntreRodas = 1;
+    private float _distanciaEntreRodas = 250;
 
     [Export]
     private float _pesoDoCarro = 1;
 
     [Export]
     private float _velocidadeMaxima = 1;
+    #endregion
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        GD.Print(GetNode("roda_esquerda"));
-        GD.Print(GetNode("roda_direita"));
+        GetNode<Sprite2D>("roda_esquerda").Position = new Vector2(0, -_distanciaEntreRodas / 2);
+        GetNode<Sprite2D>("roda_direita").Position = new Vector2(0, _distanciaEntreRodas / 2);
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
--- Scripts/roda.cs
+++ Scripts/roda.cs
@@ -3,23 +3,11 @@ using System;
 
 public partial class roda : Node2D
 {
-    private int _impulso = 0;
-    private float _velocity = 0;
-
-    private void OnImpulso(int left, int right)
-    {
-        _impulso = left;
-    }
-
     // Called when the node enters the scene tree for the first time.
-    public override void _Ready() { }
-
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta) { }
-
-    public override void _PhysicsProcess(double delta)
+    public override void _Ready()
     {
-        Rotation += _impulso * (float)delta;
-        Position += new Vector2(_impulso * _velocity, 0);
+        Position = new Vector2(300, 0);
     }
+
+    public override void _Process(double delta) { }
 }
